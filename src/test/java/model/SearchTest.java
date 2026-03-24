ee8e32c6f63dfc4951e8d9c10a73dfbc4455509c
Jimena Calvo <jimenacalvosoto13@gmail.com>
2026-03-23 19:02:59 -0600

    Parte 4

--- src/test/java/model/SearchTest.java
+++ src/test/java/model/SearchTest.java
@@ -12,8 +12,8 @@ class SearchTest {
     @Test
     void searchTest(){
 
-        int [] sortedArray = util.Utility.generatedSorted();
-        int value = new Random().nextInt(50); // Genera un valor aleatorio entre 0 y 99
+        int [] sortedArray = util.Utility.generatedSorted(20,50);
+        int value = new Random().nextInt(50); // Genera un valor aleatorio entre 0 y 50
         int pos = Search.binarySearch(sortedArray, value, 0, sortedArray.length - 1);
 
         System.out.println(Arrays.toString(sortedArray));
