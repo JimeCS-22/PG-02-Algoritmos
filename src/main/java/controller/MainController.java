2724b5ab4994703cf2952696b4900f8526d12250
Jimena Calvo <jimenacalvosoto13@gmail.com>
2026-03-25 17:59:01 -0600

    Parte 9

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -5,23 +5,35 @@ import javafx.scene.control.*;
 
 public class MainController {
     @javafx.fxml.FXML
-    private Button btnFactReset;
-    @javafx.fxml.FXML
-    private Canvas canvasTree;
-    @javafx.fxml.FXML
-    private Button btnFactCalc;
-    @javafx.fxml.FXML
-    private Label lblComplexity;
-    @javafx.fxml.FXML
-    private Slider sliderFactN;
-    @javafx.fxml.FXML
     private TabPane mainTabs;
     @javafx.fxml.FXML
-    private Label lblFactCalls;
+    private TextField txtBinValue;
     @javafx.fxml.FXML
-    private Label lblFactN;
+    private Canvas canvasBin;
     @javafx.fxml.FXML
-    private ListView listSteps;
+    private Button btnBinAnimate;
     @javafx.fxml.FXML
-    private Label lblFactResult;
+    private Button btnBinGen;
+    @javafx.fxml.FXML
+    private Label lblBinSize;
+    @javafx.fxml.FXML
+    private Slider sliderBinSize;
+    @javafx.fxml.FXML
+    private Label lblBinArray;
+    @javafx.fxml.FXML
+    private ProgressBar progressBarBin;
+    @javafx.fxml.FXML
+    private Button btnBinSearch;
+    @javafx.fxml.FXML
+    private ListView listBinSteps;
+    @javafx.fxml.FXML
+    private Label lblBinResult;
+    @javafx.fxml.FXML
+    private Label lblBinTime;
+    @javafx.fxml.FXML
+    private Label lblBinComplex;
+    @javafx.fxml.FXML
+    private Button btnBinReset;
+    @javafx.fxml.FXML
+    private Label lblBinComps;
 }
